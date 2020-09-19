5353543e736bd60efe4a2b302bf0ad0fc1cb0de9
Bhaskar <bhaskarrajaryal@gmail.com>
2020-09-19 22:06:51 +0545

    design and side navigation complete

--- app/src/main/java/com/wecoders/singleradiopro/MainActivity.java
+++ app/src/main/java/com/wecoders/singleradiopro/MainActivity.java
@@ -1,63 +1,93 @@
 package com.wecoders.singleradiopro;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Menu;
+import android.view.MenuItem;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.lifecycle.ViewModelProvider;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+import com.wecoders.singleradiopro.databinding.ActivityMainBinding;
 
-    private AppBarConfiguration mAppBarConfiguration;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+
+        com.wecoders.singleradiopro.databinding.ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        MainActivityViewModel model = new ViewModelProvider(this).get(MainActivityViewModel.class);
+        binding.setViewmodel(model);
+
+        setSupportActionBar(binding.appBarMainLayout.toolbar);
+        getSupportActionBar().setTitle("");
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, binding.drawerLayout, binding.appBarMainLayout.toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        binding.drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        binding.navView.setNavigationItemSelectedListener(this);
+        //onNavigationItemSelected(binding.navView.getMenu().getItem(0)); //select home fragment by default
+
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.nav_notification) {
+
+
+        } else if (id == R.id.nav_feedback) {
+
+
+        } else if (id == R.id.nav_pp) {
+
+
+        } else if (id == R.id.nav_rate) {
+
+            final String appPackageName = getPackageName();
+            try {
+                startActivity(new Intent(
+                        Intent.ACTION_VIEW, Uri
+                        .parse("market://details?id="
+                                + appPackageName)));
+            } catch (android.content.ActivityNotFoundException exception) {
+                startActivity(new Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://play.google.com/store/apps/details?id="
+                                + appPackageName)));
             }
-        });
+
+        } else if (id == R.id.nav_share) {
+
+            final String appPackageName = getPackageName();
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, "Download our app and listen to our radio station https://play.google.com/store/apps/details?id="
+                    + appPackageName);
+            startActivity(Intent.createChooser(shareIntent, "Share Via:"));
+
+        } else if (id == R.id.nav_about) {
+
+
+        }
+
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        drawer.closeDrawer(GravityCompat.START);
         return true;
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
---
+++ app/src/main/java/com/wecoders/singleradiopro/MainActivityViewModel.java
@@ -0,0 +1,6 @@
+package com.wecoders.singleradiopro;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainActivityViewModel  extends ViewModel {
+}
